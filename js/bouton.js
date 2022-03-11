d80abb8a564ff5226c50ccade432de3da0e699b0
Alice Botella-Vigneault <alicebotella.v@hotmail.com>
2022-03-11 03:45:29 -0500

    test js; fin texte partout

--- js/bouton.js
+++ js/bouton.js
@@ -1,7 +1,11 @@
 (function(){
+    let inverserCouleur = document.querySelectorAll('.butFoot')
 
-
-
+    inverserCouleur.addEventListener('click', function()
+        {
+            let elmBody = document.querySelector('body')
+            elmBody.classList.toggle('couleurGlobale')
+        })
 
 
 })()
